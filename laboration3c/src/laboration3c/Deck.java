b85d77bd5aaeaf5c60fe7733bfd01a3420a6dd70
Christianekenstedt <Christian@n134-p5.kthopen.kth.se>
2015-09-28 11:33:47 +0200

    Added files

---
+++ laboration3c/src/laboration3c/Deck.java
@@ -0,0 +1,71 @@
+package laboration3c;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * This class represent a card deck, built of the class Card.
+ * @author Christian Ekenstedt
+ */
+public class Deck {
+    
+    private ArrayList<Card> deck;
+    
+    /**
+     * Creates a full card deck.
+     */
+    public Deck(){ 
+        deck = new ArrayList<>();
+        fill();
+    }
+    /**
+     * Return the number of cards.
+     * @return 
+     */
+    public int noOfCards(){
+        return deck.size();
+    }
+    /**
+     * Deals a card from the top of the deck and remove it from the deck.
+     * @return 
+     */
+    public Card dealCard(){
+        Card temp = deck.get(deck.size()-1);
+        deck.remove(deck.size()-1);
+        return temp;
+    }
+    /**
+     * Shuffels the list with cards.
+     */
+    public void shuffelCards(){
+        Collections.shuffle(deck);
+    }
+    /**
+     * Remove the existing cards of the deck and the fill the deck with new cards.
+     */
+    public void fill(){
+        int n=0;
+        deck.removeAll(deck);
+        while (n<52){
+            for(int i = 0; i<4;i++){
+                for(int j = 0; j<13;j++){
+                    deck.add(new Card(j+1,i+1));
+                    n++;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Prints the card.
+     * @param card
+     * @return the string of the card.
+     */
+    public String toString(){
+        String info = new String();  
+        for(Card c: deck){
+            info += "Card: " + c.toString() + "\n";
+        }
+        return info;
+    }
+}
---
+++ laboration3c/src/laboration3c/Hand.java
@@ -0,0 +1,97 @@
+package laboration3c;
+
+import java.util.ArrayList;
+
+/**
+ * This class Hand represent the poker hand.
+ * @author Chrille
+ */
+public class Hand {
+    private ArrayList<Card> hand;
+    /**
+     * Creates a hand and init the new hand.
+     */
+    public Hand(){
+        hand = new ArrayList<>();
+    }
+    /**
+     * Return the number of cards in the hand.
+     * @return the number of cards in the hand.
+     */
+    public int getNoOfCards(){
+        return hand.size();
+    }
+    /**
+     * Adds a card to the hand and increas the noOfCards
+     * @param c
+     */
+    public void addCard(Card c){
+        hand.add(c);
+        //hand.add(card);
+      
+    }
+    /**
+     * 
+     * @param index, what position in the hand.
+     * @return the card with the position in the hand given buy the index.
+     */
+    public Card getCard(int index){
+        if((index < hand.size()) || index > 0){
+            return hand.get(index-1);
+        }else return null;
+    }
+    /**
+     * Get the total value of the hand.
+     * @return the value of the hand.
+     */
+    public int getValue(){
+        int value = 0;
+        for(int i=0; i<hand.size();i++){
+            if(hand.get(i).getRank() >10){
+                value += 10;
+            }else value += hand.get(i).getRank();
+        }
+        return value;
+    }
+    /**
+     * Removes a card in the hand.
+     * @param index, what position in the hand.
+     * @return the removed card.
+     */
+    public Card removeCard(int index){
+        Card temp;
+        if((index < hand.size()) || index > 0){
+            temp = hand.get(index-1);
+            hand.remove(index-1);
+            return temp;
+        }else return null;
+        
+    }
+    /**
+     * Reomves a given card
+     * @param c is the card to remove
+     * @return true or false if it cab ve removed.
+     */
+    public boolean removeCard(Card c){
+        
+        if (hand.contains(c)){
+            hand.remove(c);
+            return true;
+        }else return false;
+        
+    }
+    /**
+     * Return a string with all the cards in the hand.
+     * @return the hand
+     */
+    @Override
+    public String toString(){
+        String info = new String();
+        for(int i=0; i< hand.size();i++){
+            info += "Card " + (i+1) + ": " + hand.get(i) + "\n";
+        }
+        return info;
+    }
+    
+    
+}
